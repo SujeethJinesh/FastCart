047e978da305ec930d9e55e8668eb07669d9a611
Sujeeth Jinesh <admin@admins-MacBook-Pro-3.local>
2016-09-10 04:53:07 -0400

    minor changes to ItemSelected class

--- app/src/main/java/com/example/admin/fastcart/ItemSelected.java
+++ app/src/main/java/com/example/admin/fastcart/ItemSelected.java
@@ -1,10 +1,19 @@
 package com.example.admin.fastcart;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.ListView;
 
 /**
  * Created by admin on 9/10/16.
  */
 public class ItemSelected extends FragmentActivity {
 
+    private ListView listView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
