b1174ee40f63bd532efcdd06455491d9c7cfd375
Aayush Kumar <aayush.kumarmail@gmail.com>
2016-09-10 15:14:57 -0400

    google sign in working, trying to implemet swipe views

--- app/src/main/java/com/example/admin/fastcart/MainActivity.java
+++ app/src/main/java/com/example/admin/fastcart/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -32,6 +33,7 @@ public class MainActivity extends FragmentActivity implements GoogleApiClient.On
     private static final String TAG = "SignInOnMainActivity";
     private static final int RC_SIGN_IN = 9001;
     private TextView mStatusTextView;
+//    MyPageAdapter pageAdapter;
 
 
     @Override
@@ -126,6 +128,8 @@ public class MainActivity extends FragmentActivity implements GoogleApiClient.On
             integrator.setBeepEnabled(false);
             integrator.setBarcodeImageEnabled(false);
             integrator.initiateScan();
+//            ViewPager mPager = (ViewPager) findViewById(R.id.activity_screen_slide);
+//            mPager.setPageTransformer(true, new ZoomOutPageTransformer());
 //            updateUI(true);
         } else {
             // Signed out, show unauthenticated UI.
