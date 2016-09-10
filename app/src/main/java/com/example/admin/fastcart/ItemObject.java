a0d76f5d773d7baad78dc008aec527cf99223403
Sujeeth Jinesh <admin@admins-MacBook-Pro-3.local>
2016-09-10 04:26:35 -0400

    added a few classes

---
+++ app/src/main/java/com/example/admin/fastcart/ItemObject.java
@@ -0,0 +1,17 @@
+package com.example.admin.fastcart;
+
+/**
+ * Created by admin on 9/10/16.
+ */
+public class ItemObject {
+
+    private String name;
+    private String calories;
+
+    public ItemObject(String name, String calories) {
+        this.name = name;
+        this.calories = calories;
+
+    }
+
+}
---
+++ app/src/main/java/com/example/admin/fastcart/ItemSelected.java
@@ -0,0 +1,10 @@
+package com.example.admin.fastcart;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by admin on 9/10/16.
+ */
+public class ItemSelected extends FragmentActivity {
+
+}
